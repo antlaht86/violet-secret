b0d9fc569b5556fb15041d43dafe77e74660947c
antlaht86 <36437619+antlaht86@users.noreply.github.com>
2022-02-11 14:37:00 +0200

    .

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,19 +1,14 @@
-import Redis from "ioredis";
 import flatCache from "flat-cache";
-import invariant from "tiny-invariant";
 
-const cache = flatCache.load("cacheId");
+const cache = flatCache.load("cacheId", "./");
 
 export function getSecret(id: string): { pd: string; text: string } | null {
-  console.log("🤡 cache: ", cache.all());
-
   return cache.getKey(id);
 }
 
 export function setSecret(id: string, pd: string, text: string) {
   cache.setKey(id, { pd, text });
   cache.save();
-  console.log("🤡 cache: ", cache.all());
 }
 
 export function removeSecret(id: string) {
