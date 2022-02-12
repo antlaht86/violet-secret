5c94e834ac21aee64f9ab58b66c61ebe09c87ad4
antlaht86 <36437619+antlaht86@users.noreply.github.com>
2022-02-12 08:15:03 +0200

    .

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,18 +1,18 @@
 import flatCache from "flat-cache";
+import cache from "memory-cache";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const cache = flatCache.load("cacheId", join(__dirname, "_files/"));
+// const cache = flatCache.load("cacheId", join(__dirname, "_files/"));
 
 export function getSecret(id: string): { pd: string; text: string } | null {
-  return cache.getKey(id);
+  return JSON.parse(cache.get(id));
 }
 
 export function setSecret(id: string, pd: string, text: string) {
-  cache.setKey(id, { pd, text });
-  cache.save();
+  cache.put(id, JSON.stringify({ pd, text }));
 }
 
 export function removeSecret(id: string) {
-  cache.removeKey(id);
+  cache.del(id);
 }
